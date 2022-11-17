74a8ac2b68f95eae9b170ed5b5b4782a541323db
Table4 <78029398+Theophile-Wemaere@users.noreply.github.com>
2022-11-17 15:08:57 +0100

    Initial config and files

---
+++ part1_autonomous.X/ChipConfig.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   ChipConfig.h
+ * Author: TP_EO_6
+ *
+ * Created on 17 novembre 2022, 14:58
+ */
+
+#ifndef CHIPCONFIG_H
+#define	CHIPCONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define FCY 40000000
+
+
+
+void InitOscillator();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CHIPCONFIG_H */
+
---
+++ part1_autonomous.X/IO.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   IO.h
+ * Author: TP_EO_6
+ *
+ * Created on 17 novembre 2022, 15:00
+ */
+
+#ifndef IO_H
+#define	IO_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+// Prototypes fonctions
+void InitIO();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IO_H */
+
