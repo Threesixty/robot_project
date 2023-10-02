327aaba0ed1c28752b1807e8d0a9c5306928af67
Topkif <marin.dhai@gmail.com>
2023-10-02 17:31:46 +0200

    Timer 1 23 ok

--- part1_autonomous.X/timer.c
+++ part1_autonomous.X/timer.c
@@ -2,7 +2,8 @@
 #include "timer.h"
 #include "IO.h"
 
-//Initialisation d?un timer 32 bits
+//Initialisation d'un timer 32 bits
+
 void InitTimer23(void) {
     T3CONbits.TON = 0; // Stop any 16-bit Timer3 operation
     T2CONbits.TON = 0; // Stop any 16/32-bit Timer3 operation
@@ -11,8 +12,9 @@ void InitTimer23(void) {
     T2CONbits.TCKPS = 0b00; // Select 1:1 Prescaler
     TMR3 = 0x00; // Clear 32-bit Timer (msw)
     TMR2 = 0x00; // Clear 32-bit Timer (lsw)
-    PR3 = 0x0262; // Load 32-bit period value (msw)
-    PR2 = 0x5A00; // Load 32-bit period value (lsw)
+    // period = T1CK/Prescaler/0.5Hz = 0x4C4 B400
+    PR3 = 0x04C4; // Load 32-bit period value (msw)
+    PR2 = 0x1200; // Load 32-bit period value (lsw)
     IPC2bits.T3IP = 0x01; // Set Timer3 Interrupt Priority Level
     IFS0bits.T3IF = 0; // Clear Timer3 Interrupt Flag
     IEC0bits.T3IE = 1; // Enable Timer3 interrupt
@@ -21,30 +23,35 @@ void InitTimer23(void) {
 }
 
 //Interruption du timer 32 bits sur 2-3
+
 void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
     IFS0bits.T3IF = 0; // Clear Timer3 Interrupt Flag
     LED_ORANGE = !LED_ORANGE;
 }
 
-//Initialisation d?un timer 16 bits
-void InitTimer1(void)
-{
+//Initialisation d'un timer 16 bits
+
+void InitTimer1(void) {
     //Timer1 pour horodater les mesures (1ms)
-    T1CONbits.TON = 0; // Disable Timer
+    T1CONbits.TON = 0; // Stop Timer
+    T1CONbits.TCS = 0; //clock source = internal clock
     T1CONbits.TCKPS = 0b01; //Prescaler
     //11 = 1:256 prescale value
     //10 = 1:64 prescale value
     //01 = 1:8 prescale value
     //00 = 1:1 prescale value
-    T1CONbits.TCS = 0; //clock source = internal clock
-    PR1 = 0x1388;
+
+    //    PR1 = 5000; // frequency = (T1CK/prescaler)/ 5000 = 1000 Hz
+    PR1 = 50000; // maxVal = (T1CK/prescaler de 8)/ 100 Hz = 50 000
 
     IFS0bits.T1IF = 0; // Clear Timer Interrupt Flag
     IEC0bits.T1IE = 1; // Enable Timer interrupt
     T1CONbits.TON = 1; // Enable Timer
 }
 
+
 //Interruption du timer 1
+
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
     IFS0bits.T1IF = 0;
     LED_BLANCHE = !LED_BLANCHE;
--- part1_autonomous.X/timer.h
+++ part1_autonomous.X/timer.h
@@ -1,15 +1,8 @@
-/* 
- * File:   timer.h
- * Author: TP_EO_6
- *
- * Created on 17 novembre 2022, 17:31
- */
-
 #ifndef TIMER_H
 #define	TIMER_H
 
-void InitTimer23(void) ;
-void InitTimer1(void) ;
+void InitTimer23(void);
+void InitTimer1(void);
 
 #endif	/* TIMER_H */
 
